56773018021679ec2ad49eabb7d2ca37dc7f5b7f
Spooky <73013941+microbullet@users.noreply.github.com>
2023-01-29 18:46:06 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from discord import app_commands
 import os
 
 
+punkcan = 535849677858275329
+gid = 883968176155664405
+
 class aclient(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
@@ -11,7 +14,7 @@ class aclient(discord.Client):
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced:
-            await tree.sync(guild=discord.Object(id=883968176155664405))
+            await tree.sync(guild=discord.Object(id=gid))
             self.synced = True
         print(f"We have logged in as {self.user}.")
 
@@ -19,10 +22,17 @@ class aclient(discord.Client):
 client = aclient()
 tree = app_commands.CommandTree(client)
 
+math = client.create_group(
+    "math", "Commands related to mathematics."
+) 
 
-@tree.command(name="test", description="testing", guild=discord.Object(id=883968176155664405))
+@tree.command(name="test", description="testing", guild=discord.Object(id=gid))
 async def self(interaction: discord.Interaction, name: str):
     await interaction.response.send_message(f"Hello {name}! I was made with Discord.py!")
+
+@tree.command(name="ping", description="pong", guild=discord.Object(id=gid))
+async def self(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong")
 
 
 client.run(os.environ["DISCORD_TOKEN"])
