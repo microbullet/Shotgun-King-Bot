f51338a0b82813a2db8015d067b088de0c76d7e8
Spooky <73013941+microbullet@users.noreply.github.com>
2023-01-29 18:19:54 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import discord
 from discord import app_commands
 import os
 
+
 class aclient(discord.Client):
     def __init__(self):
-        super().__init__(intents=discord. Intents.default())
+        super().__init__(intents=discord.Intents.default())
         self.synced = False
 
     async def on_ready(self):
@@ -12,13 +13,16 @@ class aclient(discord.Client):
         if not self.synced:
             await tree.sync(guild=discord.Object(id=908141823288045568))
             self.synced = True
-        print (f"We have logged in as {self.user}.")
+        print(f"We have logged in as {self.user}.")
+
 
 client = aclient()
 tree = app_commands.CommandTree(client)
 
-@tree.command (name="test", description="testing", guild=discord.Object(id = 908141823288045568))
-async def self(interaction: discord. Interaction, name: str):
+
+@tree.command(name="test", description="testing", guild=discord.Object(id=908141823288045568))
+async def self(interaction: discord.Interaction, name: str):
     await interaction.response.send_message(f"Hello {name}! I was made with Discord.py!")
+
 
 client.run(os.environ["DISCORD_TOKEN"])
