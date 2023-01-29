34f9909d40fb6af6ab12158fd05b79d64f465916
Spooky <73013941+microbullet@users.noreply.github.com>
2023-01-29 14:58:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,6 @@ async def ping(ctx):
 
 @client.command(description="Sends the bot's latency.") # this decorator makes a slash command
 async def ping(ctx): # a slash command will be created with the name "ping"
-    await ctx.respond(f"Pong! Latency is {bot.latency}")
+    await ctx.respond(f"Pong! Latency is {client.latency}")
 
 client.run(os.environ["DISCORD_TOKEN"])
