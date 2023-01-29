92924defcd68b4ad508aa565bf44cd4875d2cbd7
Spooky <73013941+microbullet@users.noreply.github.com>
2023-01-29 17:34:28 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import discord
 from discord import app_commands
 
 intents = discord.Intents.default()
-client = discord.Client(intents=intents, guild_id=id=883968176155664405)
+client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 @tree.command(name = "commandname", description = "My first application Command", guild=discord.Object(id=883968176155664405)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
