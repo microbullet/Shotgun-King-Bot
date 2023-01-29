4d23bd0d2bc3f649f3b1c1d20e59369bae18dc77
Spooky <73013941+microbullet@users.noreply.github.com>
2023-01-29 17:53:11 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,76 @@
-# This example requires the 'message_content' privileged intents
-
-import os
 import discord
-from discord import app_commands
+import os
+from discord.ext import commands
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
 
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
+intents.members = True
+intents.message_content = True
 
-@tree.command(name = "commandname", description = "My first application Command", guild=discord.Object(id=883968176155664405)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-async def first_command(interaction):
-    await interaction.response.send_message("Hello!")
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
-async def sync():
-    await tree.sync(guild=discord.Object(id=883968176155664405))
-    
 
-@client.event
+@bot.event
 async def on_ready():
-    print("Ready!")
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
-client.run(os.environ["DISCORD_TOKEN"])
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+
+bot.run(os.environ["DISCORD_TOKEN"])
