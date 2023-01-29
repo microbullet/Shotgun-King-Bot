6c3a9875af36e8acaf54fd1385e83fbc40e8746d
Spooky <73013941+microbullet@users.noreply.github.com>
2023-01-29 20:47:25 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,26 @@ import discord
 from discord import app_commands
 import os
 
-
 punkcan = 535849677858275329
 gid = 883968176155664405
 
-class aclient(discord.Client):
-    def __init__(self):
-        super().__init__(intents=discord.Intents.default())
-        self.synced = False
+bot = discord.Bot()
 
-    async def on_ready(self):
-        await self.wait_until_ready()
-        if not self.synced:
-            await tree.sync(guild=discord.Object(id=gid))
-            self.synced = True
-        print(f"We have logged in as {self.user}.")
+# If you use commands.Bot, @bot.slash_command should be used for
+# slash commands. You can use @bot.slash_command with discord.Bot as well.
+
+random = bot.create_group(
+    "random", "gives a random thing based on the sub"
+)
 
 
-client = aclient()
-tree = app_commands.CommandTree(client)
+@random.command()  # Create a slash command under the math group
+async def add(ctx: discord.ApplicationContext, num1: int, num2: int):
+    """Get the sum of 2 integers."""
+    await ctx.respond(f"The sum of these numbers is **{num1+num2}**")
 
 
-@tree.command(name="test", description="testing", guild=discord.Object(id=gid))
-async def self(interaction: discord.Interaction, name: str):
-    await interaction.response.send_message(f"Hello {name}! I was made with Discord.py!")
-
-@tree.command(name="pingg", description="pong", guild=discord.Object(id=gid))
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message("Pong")
+bot.add_application_command(math)
 
 
-client.run(os.environ["DISCORD_TOKEN"])
+bot.run(os.environ["DISCORD_TOKEN"])
