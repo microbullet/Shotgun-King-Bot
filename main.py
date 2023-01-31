78f3629cfd52c38f63365a6d65681d165e097173
Spooky <73013941+microbullet@users.noreply.github.com>
2023-01-31 14:09:08 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from discord.ext import commands
 import os
 
 bot = discord.Bot(command_prefix='!', intents=discord.Intents.default())
-attributes = dir(bot)
-print(attributes)
 
 punkcan = 535849677858275329
 gid = 883968176155664405
@@ -48,4 +46,3 @@ async def subcommand(ctx):
 
 if __name__ == "__main__": 
     client.run(os.environ["DISCORD_TOKEN"])
-    
