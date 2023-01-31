d028dcf6d84d144367505897ea8409d78fa34a91
Spooky <73013941+microbullet@users.noreply.github.com>
2023-01-31 18:39:27 +1000

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ math = discord.SlashCommandGroup("math", "Commands related to mathematics.")
 @math.command()  # Create a slash command under the math group
 async def add(ctx: discord.ApplicationContext, num1: int, num2: int):
     """Get the sum of 2 integers."""
-    await ctx.respond(f"The sum of these numbers is **{num1+num2}**")
+    await ctx.respond(f"The sum o these numbers is **{num1+num2}**")
 
 
 bot.add_application_command(math)
