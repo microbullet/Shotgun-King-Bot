3172cb03984dd08c408dc3d2a66aaf3c9db052ae
Spooky <73013941+microbullet@users.noreply.github.com>
2023-01-31 14:41:48 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
+import os
 
-bot = discord.Bot(debug_guilds=[...])
+bot = discord.Bot(command_prefix="!")
 
 # If you use commands.Bot, @bot.slash_command should be used for
 # slash commands. You can use @bot.slash_command with discord.Bot as well.
